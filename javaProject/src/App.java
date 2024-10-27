8b1c8cd2c85e58bc05e10f5cbc280299336d8325
KLAUS-GBHM <146330604+XXXXKLAUSXXX@users.noreply.github.com>
2024-10-27 22:42:10 +0100

    Added unit testing stuffs

--- javaProject/src/App.java
+++ javaProject/src/App.java
@@ -1,6 +1,97 @@
+import java.util.*;
 
 public class App {
-    public static void main(String [] args){
+    static boolean shout = true; //if true tests will print their output.
+
+    public static void main(String[] args) {
         System.out.println("hiiii");
+        Test.testExample(shout);
     }
+}
+
+class Test { //
+
+    static void newTests(String testID) {
+        System.out.println("\nNow testing\t:" + testID + "\n");
+
+        return;
+    }
+
+    static void exp(String expecting) {
+        System.out.println("Expecting\t:" + expecting);
+        return;
+    }
+
+    static void exp(int expecting) {
+        System.out.println("Expecting\t:" + expecting);
+        return;
+    }
+
+    static void res(String expecting) {
+        System.out.println("\tRecieved\t:" + expecting);
+        return;
+    }
+
+    static void res(int expecting) {
+        System.out.println("\tRecieved\t:" + expecting);
+        return;
+    }
+
+    static void exp(String expecting, String received) {
+        System.out.println("Expecting\t:" + expecting + "\t\t\treceived\t:" + received);
+        return;
+    }
+
+    static void exp(String expecting, int received) {
+        System.out.println("Expecting\t:" + expecting + "\t\t\treceived\t:" + received);
+        return;
+    }
+
+
+    static int testExample(boolean shout) { // returns amount of errors
+        int errors = 0;
+
+        //explain test here
+        // testing the testing functions, and making an example template.
+        if (shout) newTests("Example");
+
+//testing zone
+        //setup unit test, classes, instances, etc
+        String testThing;   //testing variables are important
+        testThing = Integer.toString(1) + Integer.toString(2) + Integer.toString(3);
+
+        //test condition of this unit
+        if (!testThing.equals("123")) errors++; // if my condition isn't true, increment errors
+        if (shout) exp("123", testThing);
+
+        //next unit
+
+//shout the amount of errors
+        if (shout) exp("errors", errors);
+
+//end of testing zone
+        return errors;
+    }//end of test
+
+    static int testBlank(boolean shout) { // returns amount of errors
+        int errors = 0;
+
+        //explain test here
+        // testing the testing functions, and making an example template.
+        if (shout) newTests("");
+
+//testing zone
+        //setup unit test, classes, instances, etc
+
+        //test condition of this unit
+
+        //next unit
+
+//shout the amount of errors
+        if (shout) exp("errors", errors);
+
+//end of testing zone
+        return errors;
+    }//end of test
+
 }
