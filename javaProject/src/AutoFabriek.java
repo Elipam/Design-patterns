c78265fddd0deb0b7d05ad56deb2ecb4018c696c
kristifristi <Kristiaancramer@gmail.com>
2024-10-30 15:13:12 +0100

    alsof het nooit was gebeurt

--- javaProject/src/AutoFabriek.java
+++ javaProject/src/AutoFabriek.java
@@ -5,7 +5,7 @@ abstract public class AutoFabriek {
     public abstract Auto MaakAuto();
    //DIT IS NIET HOE JE EEN FACTORY GEBRUIKT
    //de veel to complexe constructor moet juist niet zichtbaar zijn voor de gebruiker
-   // public abstract Auto MaakAuto(AandrijvingSelectie aandrijving, RemSysteem remsysteem, int bouwjaar, String kleur);
+   // public abstract Auto MaakAuto(Motor motor, RemSysteem remsysteem, int bouwjaar, String kleur);
 
 }
 
@@ -27,16 +27,5 @@ class BestelBusAutoFabriek extends AutoFabriek {
     @Override
     public Auto MaakAuto() {
         return new BestelBus();
-    }
-}
-
-class AandrijfTest extends AutoFabriek {
-    private int versie = 0;
-    private AandrijvingSelectie[] selectie = {new DieselSelectie(), new ElektrischeSelectie(),new BenzineSelectie()};
-    @Override
-    public Auto MaakAuto() {
-        Auto nieuw = new Aandrijver(selectie[versie].leverMotor(),selectie[versie].leverTank());
-        versie = (versie + 1) % 3;
-        return nieuw;
     }
 }
