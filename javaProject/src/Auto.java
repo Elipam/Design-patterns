fd1c145d07d84ab211fbb7e90e8e0108340aae13
Eliam Traas <eliamtraas@gmail.com>
2024-10-30 15:32:51 +0100

    Auto en Autodecorater toegevoegd, andere klassen uitgecomment tot ze werken

--- javaProject/src/Auto.java
+++ javaProject/src/Auto.java
@@ -1,22 +1,23 @@
 public abstract class Auto {
-    protected Motor moter;
+    // Commented out for now
+    /*protected Motor moter;
     protected RemSysteem remSystem;
-
-    protected Tank tank;
+    protected Tank tank;*/
 
     protected String kleur;
     private int bouwjaar;
-    private float gewicht;  //kg     //float, that precision could be important. And some cars might be heavier then an int can express.
-    private float price;    //float, some cars might cost more then int can express.
+    private float gewicht;
+    private float price;
 
-    //methods
     public Auto() {
     }
 
-    public Auto(Motor moter, RemSysteem remSystem, Tank tank, String kleur, int bouwjaar, float gewicht, float price) {
-        this.moter = moter;
+    // Simplified constructor
+    public Auto(/*Motor moter, RemSysteem remSystem, Tank tank,*/ 
+                String kleur, int bouwjaar, float gewicht, float price) {
+        /*this.moter = moter;
         this.remSystem = remSystem;
-        this.tank = tank;
+        this.tank = tank;*/
         this.kleur = kleur;
         this.bouwjaar = bouwjaar;
         this.gewicht = gewicht;
@@ -24,32 +25,82 @@ public abstract class Auto {
     }
 
     public void start() {
-        moter.startMotor(tank);
+        // Simplified for now
+        System.out.println("Auto starting");
+        /*moter.startMotor(tank);*/
     }
 
     public void stop() {
+        System.out.println("Auto stopping");
     }
 
-    public float getPrijs() {
-        return 0;
-    }
+    public abstract float getPrijs();
 
     public void printInfo() {
-        System.out.println(kleur + tank.TankType + tank.getEnergieType() + remSystem.naam);
+        System.out.println(kleur + " auto");
+        /*System.out.println(kleur + tank.TankType + tank.getEnergieType() + remSystem.naam);*/
     }
 }
 
 class SportAuto extends Auto {
+    private static final float BASE_PRICE = 50000.0f;
+    
+    public SportAuto() {
+        super(/*new Motor(), new RemSysteem(), new Tank(),*/ 
+              "Rood", 2024, 1200.0f, BASE_PRICE);
+    }
+
+    @Override
+    public float getPrijs() {
+        return BASE_PRICE;
+    }
+
+    @Override
+    public void printInfo() {
+        System.out.println("SportAuto: €" + getPrijs());
+    }
 }
 
 class PersonenAuto extends Auto {
+    private static final float BASE_PRICE = 20000.0f;
+    
+    public PersonenAuto() {
+        super(/*new Motor(), new RemSysteem(), new Tank(),*/ 
+              "Zwart", 2024, 1500.0f, BASE_PRICE);
+    }
+
+    @Override
+    public float getPrijs() {
+        return BASE_PRICE;
+    }
+
+    @Override
+    public void printInfo() {
+        System.out.println("PersonenAuto: €" + getPrijs());
+    }
 }
 
 class BestelBus extends Auto {
-}
+    private static final float BASE_PRICE = 35000.0f;
+    
+    public BestelBus() {
+        super(/*new Motor(), new RemSysteem(), new Tank(),*/ 
+              "Wit", 2024, 2500.0f, BASE_PRICE);
+    }
 
-class Aandrijver extends Auto {
-    public Aandrijver(Motor moter, Tank tank) {
-        super(moter, null, tank, "", 0, 0, 0);
+    @Override
+    public float getPrijs() {
+        return BASE_PRICE;
+    }
+
+    @Override
+    public void printInfo() {
+        System.out.println("BestelBus: €" + getPrijs());
     }
 }
+
+// class Aandrijver extends Auto {
+//     public Aandrijver(Motor moter, Tank tank) {
+//         super(moter, null, tank, "", 0, 0, 0);
+//     }
+// }
--- javaProject/src/AutoDecorator.java
+++ javaProject/src/AutoDecorator.java
@@ -1,64 +1,114 @@
-public abstract class AutoDecorator {
+public abstract class AutoDecorator extends Auto {
     protected Auto auto;
 
     public AutoDecorator(Auto auto) {
-
+        this.auto = auto;
     }
 
-    public AutoDecorator() {
-
+    @Override
+    public void start() {
+        auto.start();
     }
 
-    abstract public void start();
-
-    abstract public void stop();
-
-    public int getPrijs() {
-        return 0;
+    @Override
+    public void stop() {
+        auto.stop();
     }
 
-    public void printinfo() {
+    @Override
+    public float getPrijs() {
+        return auto.getPrijs();
+    }
+
+    @Override
+    public void printInfo() {
+        auto.printInfo();
     }
 }
-
 class ParkingSensor extends AutoDecorator {
-
-
     public ParkingSensor(Auto auto) {
+        super(auto);  // Roep constructor van parent
     }
 
+    @Override
     public void start() {
+        super.start();  // Roep methode van parent
+        System.out.println("Parking sensors activated");
     }
 
+    @Override
     public void stop() {
+        super.stop();
+        System.out.println("Parking sensors deactivated");
     }
 
+    @Override
+    public float getPrijs() {
+        return super.getPrijs() + 500;
+    }
+
+    @Override
+    public void printInfo() {
+        super.printInfo();
+        System.out.println("+ Parking sensors (€500)");
+    }
 }
 
 class BosseSound extends AutoDecorator {
-
-
     public BosseSound(Auto auto) {
+        super(auto);
     }
 
+    @Override
     public void start() {
+        super.start();
+        System.out.println("Bosse sound system online");
     }
 
+    @Override
     public void stop() {
+        super.stop();
+        System.out.println("Bosse sound system offline");
     }
 
+    @Override
+    public float getPrijs() {
+        return super.getPrijs() + 1500;
+    }
+
+    @Override
+    public void printInfo() {
+        super.printInfo();
+        System.out.println("+ Bosse Sound System (€1500)");
+    }
 }
 
 class HeatedSeats extends AutoDecorator {
 
-
     public HeatedSeats(Auto auto) {
+        super(auto);  // Call parent constructor
     }
 
+    @Override
     public void start() {
+        super.start();  // Call parent method
+        System.out.println("Heated seats warming up");
     }
 
+    @Override
     public void stop() {
+        super.stop();
+        System.out.println("Heated seats turned off");
     }
 
+    @Override
+    public float getPrijs() {
+        return super.getPrijs() + 750;
+    }
+
+    @Override
+    public void printInfo() {
+        super.printInfo();
+        System.out.println("+ Heated Seats (€750)");
+    }
 }
